3aed2535b9d3b30040126fa933821f40f7094068
Ludovic Chabant <ludovic@chabant.com>
2023-10-28 13:06:34 -0700

    Upgrade webmention silo to new API

--- silorider/silos/webmention.py
+++ silorider/silos/webmention.py
@@ -2,6 +2,7 @@ import logging
 import ronkyuu
 from .base import Silo
 from ..config import has_lxml
+from ..format import CardInfo
 
 
 logger = logging.getLogger(__name__)
@@ -18,8 +19,11 @@ class WebmentionSilo(Silo):
     def authenticate(self, ctx):
         logger.info("Webmention silo doesn't require authentication.")
 
-    def postEntry(self, entry, ctx):
-        source_url = entry.url
+    def getEntryCard(self, entry, ctx):
+        return CardInfo(entry, "", None)
+
+    def postEntry(self, entry_card, media_ids, ctx):
+        source_url = entry_card.entry.url
         logger.debug("Finding mentions in: %s" % source_url)
         refs = ronkyuu.findMentions(source_url)
         for r in refs.get('refs', []):
